3aa88bb7fc7c9ceb78213183442c24c3e2b278b6
Simon_Schaefer <ssimonbe@students.uni-mainz.de>
2023-06-05 20:37:00 +0200

    adding exclusion function to tukey

--- R/tukey.R
+++ R/tukey.R
@@ -3,7 +3,7 @@
 #' @param data data frame to apply the tukey outlier analysis on.
 #' @param dv variable to apply the tukey outlier analysis on.
 #' @param tukey_crit factor of the IQR that defines an outlier.
-#'
+#' @param exclude logical indicating if outliers should be excluded (default = FALSE).
 #' @return data frame including new variables concerning tukey outlier analysis.
 #' @export
 #'
@@ -15,7 +15,7 @@
 #' @import tidyr
 
 
-tukey <- function(data,dv,tukey_crit=3){
+tukey <- function(data,dv,tukey_crit=3,exclude = F){
 
   dv_ <- rlang::enexpr(dv)
   dv_str <- rlang::as_string(dv_)
@@ -23,6 +23,14 @@ tukey <- function(data,dv,tukey_crit=3){
   data <- data %>%
     dplyr::mutate(tidyr::as_tibble(tukey_outlier_fun(dv=!!sym(dv_str),
                                                      tukey_crit=tukey_crit)))
+
+  if(exclude){
+
+    data <- data %>%
+      filter(!!sym(paste0(dv_str,"_outlierTukeyHigh")) != 1 &
+               !!sym(paste0(dv_str,"_outlierTukeyLow")) != 1 )
+  }
+
 
   return(data)
 
