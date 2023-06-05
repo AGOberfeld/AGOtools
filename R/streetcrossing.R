12196a234b1dda9b8968e1ee8863b709988111f9
Simon_Schaefer <ssimonbe@students.uni-mainz.de>
2023-06-05 21:02:37 +0200

    adding streetcrossing data to README

---
+++ R/streetcrossing.R
@@ -0,0 +1,17 @@
+#' Data from Streetcrossing Experiment 2022
+#'
+#' @format ## `streecrossing`
+#' A data frame with 2,771 rows and 10 columns:
+#' \describe{
+#'   \item{vp_code}{participant identification code}
+#'   \item{label}{Presented car}
+#'   \item{modality}{Modality (A = auditory, AV = audiovisual)}
+#'   \item{v0}{}
+#'   \item{a}{}
+#'   \item{gain}{}
+#'   \item{track_TTC}{}
+#'   \item{nTrials}{Number of trials}
+#'   \item{nCross}{velocity at occlusion}
+#'
+#' }
+"streetcrossing"
