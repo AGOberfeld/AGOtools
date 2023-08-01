f4a6a17faec4cf8f1457f4d6c96bc43ed708c1f3
DanielOberfeld <113097768+DanielOberfeld@users.noreply.github.com>
2023-08-01 14:52:43 +0200

    Added shapes

--- R/plotThemeAGO.R
+++ R/plotThemeAGO.R
@@ -41,7 +41,7 @@ plotThemeAGO <- function(base_size=14, base_family="Arial Unicode MS") {
             legend.title = element_text(face="italic",size=rel(0.8)),
             strip.background = element_blank(),
             strip.text = element_text(face="bold")),
-     scale_shape_manual(values = c(15, 1, 17, 5))
+     scale_shape_manual(values = c(15, 1, 17, 5,2,5,7))
     )
 
 }
