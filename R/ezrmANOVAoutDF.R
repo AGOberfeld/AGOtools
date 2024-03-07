0a0adca88ed1f7c1d583042fec23b64fc159985c
Oberfeld-Twistel <oberfeld@uni-mainz.de>
2024-03-07 08:40:32 +0100

    update ezrmANOVAoutDF

--- R/ezrmANOVAoutDF.R
+++ R/ezrmANOVAoutDF.R
@@ -12,7 +12,7 @@ ezrmANOVAoutDF=function(ezANOVAobj){
   if (exists("Sphericity Corrections",where=ezANOVAobj)) { #add sphericity corrections if applicable
     HFGG=ezANOVAobj$`Sphericity Corrections` %>% as.data.frame()
     rmANOVAout=left_join(ANOVA,HFGG,by=join_by(Effect))
-    rmANOVAout = rmANOVAout %>% mutate(`p[HF]` = ifelse(is.na(`p[HF]`), `p`, `p[HF]`),`p[HF]` = ifelse(is.na(`p[GG]`), `p`, `p[GG]`))
+    rmANOVAout = rmANOVAout %>% mutate(`p[HF]` = ifelse(is.na(`p[HF]`), p, `p[HF]`),`p[GG]` = ifelse(is.na(`p[GG]`), p, `p[GG]`))
   } else
   {rmANOVAout=ANOVA}
 
