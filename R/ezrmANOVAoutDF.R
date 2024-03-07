c499901013bc6af8eaff59a55ee4f382d90e94e5
Oberfeld-Twistel <oberfeld@uni-mainz.de>
2024-03-07 08:31:56 +0100

    update ezrmANOVAoutDF

--- R/ezrmANOVAoutDF.R
+++ R/ezrmANOVAoutDF.R
@@ -2,16 +2,17 @@
 #'
 #' @param ezANOVAobj the input ezANOVA object
 #'
-#' @return rmANOVAout dataframe with relevant statistics per effect
+#' @return rmANOVAout: dataframe with relevant statistics per effect
 #' @export
 #' @import dplyr
 
 ezrmANOVAoutDF=function(ezANOVAobj){
-  ezANOVAobj$ANOVA$etaSq_partial <- ezANOVAobj$ANOVA$SSn/(ezANOVAobj$ANOVA$SSn+ezANOVAobj$ANOVA$SSd)
+  ezANOVAobj$ANOVA$etaSq_partial <- ezANOVAobj$ANOVA$SSn/(ezANOVAobj$ANOVA$SSn+ezANOVAobj$ANOVA$SSd) # compute partial eta squared
   ANOVA=ezANOVAobj$ANOVA %>% as.data.frame()
   if (exists("Sphericity Corrections",where=ezANOVAobj)) { #add sphericity corrections if applicable
     HFGG=ezANOVAobj$`Sphericity Corrections` %>% as.data.frame()
     rmANOVAout=left_join(ANOVA,HFGG,by=join_by(Effect))
+    rmANOVAout = rmANOVAout %>% mutate(`p[HF]` = ifelse(is.na(`p[HF]`), `p`, `p[HF]`),`p[HF]` = ifelse(is.na(`p[GG]`), `p`, `p[GG]`))
   } else
   {rmANOVAout=ANOVA}
 
